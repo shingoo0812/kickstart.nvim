6670f8cb91273df40dafc892ecee92c0a42e0ffb
wowthatsabigturtle <110785810+wowthatsabigturtle@users.noreply.github.com>
2024-05-05 08:03:55 -0700

    Add external editor Godot config

--- init.lua
+++ init.lua
@@ -926,6 +926,8 @@ if vim.fn.filereadable(vim.fn.getcwd() .. '/project.godot') == 1 then
   local addr = './godot.pipe'
   if vim.fn.has 'win32' == 1 then
     -- Windows can't pipe so use localhost. Make sure this is configured in Godot
+    -- Exec Path: nvim
+    -- Exec Flags: --server 127.0.0.1:6004 --remote-send "<esc>:n {file}<CR>:call cursor({line},{col})<CR>"
     addr = '127.0.0.1:6004'
   end
   vim.fn.serverstart(addr)
