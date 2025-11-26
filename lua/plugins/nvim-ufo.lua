573d04b825ab7580a2cf75c2f4b5cb61f396c416
shingoo0812 <shingoo0812@gmail.com>
2025-11-26 20:17:59 +0900

    add ufo keys

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -2,6 +2,36 @@ return {
   {
     'kevinhwang91/nvim-ufo',
     dependencies = 'kevinhwang91/promise-async',
+    keys = {
+      {
+        'zR',
+        function()
+          require('ufo').openAllFolds()
+        end,
+        desc = 'Open all folds',
+      },
+      {
+        'zM',
+        function()
+          require('ufo').closeAllFolds()
+        end,
+        desc = 'Close all folds',
+      },
+      {
+        'zr',
+        function()
+          require('ufo').openFoldsExceptKinds()
+        end,
+        desc = 'Open folds except kinds',
+      },
+      {
+        'zm',
+        function()
+          require('ufo').closeFoldsWith()
+        end,
+        desc = 'Close folds with',
+      },
+    },
     config = function()
       -- UFOセットアップ
       require('ufo').setup {
