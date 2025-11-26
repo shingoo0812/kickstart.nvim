5f01d33ca2570adf80e8ffaec8ece637c6c9b38d
shingoo0812 <shingoo0812@gmail.com>
2025-11-26 20:50:08 +0900

    modify lazygit command to open current project folder

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -12,6 +12,21 @@ return {
   },
   keys = {
     { '<leader>g', '', desc = 'Git & Glance' },
-    { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    {
+      '<leader>gg',
+      function()
+        local util = require 'lspconfig.util'
+        local bufpath = vim.api.nvim_buf_get_name(0)
+        local root = util.root_pattern '.git'(bufpath)
+
+        if not root or root == '' then
+          root = vim.fn.getcwd()
+        end
+
+        -- LazyGit の UI をディレクトリ指定で呼び出す
+        require('lazygit').lazygit(root)
+      end,
+      desc = 'LazyGit (Open at Project Root)',
+    },
   },
 }
