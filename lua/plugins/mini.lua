043d628361ebfb926c8887b56edb11ced4b8b65e
shingoo0812 <shingoo0812@gmail.com>
2025-11-26 20:48:55 +0900

    add comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -32,6 +32,8 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
+    -- - vai - [V]isually select [A]round [I]ndent
+    -- - vii - [V]isually select [I]nside [I]ndent
     require('mini.surround').setup()
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
