eecc2577713a21c4635e61a59828050a9ab395b7
shingoo0812 <shingoo0812@gmail.com>
2025-11-27 14:03:47 +0900

    modify to search current file project

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -1,46 +1,67 @@
+local fn = require 'config.functions'
+local git_root = fn.functions.utils.git_root
+
 return {
   'MagicDuck/grug-far.nvim',
   keys = {
     -- ノーマルモード：通常検索
     {
       '<leader>.',
+      function() end,
+      desc = 'GrugFar Search',
+      mode = 'n',
+    },
+    {
+      '<leader>.s',
       function()
-        require('grug-far').search()
+        require('grug-far').search {
+          prefills = { paths = git_root() },
+        }
       end,
       desc = 'GrugFar Search',
       mode = 'n',
     },
+
     -- ノーマルモード：検索・置換 UI
     {
       '<leader>.r',
       function()
-        require('grug-far').open()
+        require('grug-far').open {
+          prefills = { paths = git_root() },
+        }
       end,
       desc = 'GrugFar Replace',
       mode = 'n',
     },
+
     -- ビジュアルモード：選択テキストを検索・置換
     {
-      '<leader>.r',
+      '<leader>.v',
       function()
-        require('grug-far').with_visual_selection()
+        require('grug-far').with_visual_selection {
+          prefills = { paths = git_root() },
+        }
       end,
       desc = 'Search and Replace (selection)',
       mode = 'v',
     },
+
     -- ノーマルモード：カーソル下の単語を検索
     {
       '<leader>.w',
       function()
-        require('grug-far').open { prefills = { search = vim.fn.expand '<cword>' } }
+        require('grug-far').open {
+          prefills = { paths = git_root(), search = vim.fn.expand '<cword>' },
+        }
       end,
       desc = 'Search and Replace (word under cursor)',
       mode = 'n',
     },
   },
+
   config = function()
     require('grug-far').setup {
-      windowCreationCommand = 'vsplit', -- 縦分割で開く場合は "split"
+      windowCreationCommand = 'vsplit',
       minSearchChars = 2,
       debounceMs = 500,
     }
