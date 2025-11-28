e89b1465ffe6809503c65b2faf1e9bb5dd494766
shingoo0812 <shingoo0812@gmail.com>
2025-11-28 15:41:38 +0900

    modify todo for fzf

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,7 +1,10 @@
 -- Highlight todo, notes, etc in comments
 return {
-  'folke/todo-comments.nvim',
-  event = 'VimEnter',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false, lazy = true },
+  {
+    'folke/todo-comments.nvim',
+    enabled = false,
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons', 'ibhagwan/fzf-lua' },
+    opts = { signs = false, lazy = true },
+  },
 }
