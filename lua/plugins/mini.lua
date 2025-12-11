70ac2b118f96ee5a6be9ec2230b2d7795bb85a45
shingoo0812 <shingoo0812@gmail.com>
2025-12-11 13:44:13 +0900

    add mini cmdline

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -49,7 +49,47 @@ return { -- Collection of various small independent plugins/modules
     statusline.section_location = function()
       return '%2l:%-2v'
     end
+    require('mini.cmdline').setup {
+      -- Autocompletion: show `:h 'wildmenu'` as you type
+      autocomplete = {
+        enable = true,
 
+        -- Delay (in ms) after which to trigger completion
+        -- Neovim>=0.12 is recommended for positive values
+        delay = 0,
+
+        -- Custom rule of when to trigger completion
+        predicate = nil,
+
+        -- Whether to map arrow keys for more consistent wildmenu behavior
+        map_arrows = true,
+      },
+
+      -- Autocorrection: adjust non-existing words (commands, options, etc.)
+      autocorrect = {
+        enable = true,
+
+        -- Custom autocorrection rule
+        func = nil,
+      },
+
+      -- Autopeek: show command's target range in a floating window
+      autopeek = {
+        enable = true,
+
+        -- Number of lines to show above and below range lines
+        n_context = 1,
+
+        -- Window options
+        window = {
+          -- Floating window config
+          config = {},
+
+          -- Function to render statuscolumn
+          statuscolumn = nil,
+        },
+      },
+    }
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
