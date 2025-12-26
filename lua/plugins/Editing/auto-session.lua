f9e60c92d8ffe6a76c65667ded1f31b54ee8ff78
shingoo0812 <shingoo0812@gmail.com>
2025-12-26 14:01:23 +0900

    modify auto-session

--- lua/plugins/Editing/auto-session.lua
+++ lua/plugins/Editing/auto-session.lua
@@ -4,6 +4,25 @@ return {
     require('auto-session').setup {
       auto_restore_enabled = true,
       auto_save_enabled = true,
+      post_restore_cmds = {
+        function()
+          vim.defer_fn(function()
+            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+              if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_name(buf) ~= '' then
+                vim.api.nvim_buf_call(buf, function()
+                  vim.cmd 'filetype detect'
+                  vim.cmd 'edit'
+                end)
+              end
+            end
+          end, 100)
+        end,
+      },
+      post_open_cmds = {
+        function()
+          vim.cmd 'filetype detect'
+        end,
+      },
     }
   end,
 }
