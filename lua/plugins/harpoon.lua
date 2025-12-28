4452b97df3410a265fbaec5b2a97c250cc00bd7a
shingoo0812 <shingoo0812@gmail.com>
2025-12-28 14:21:44 +0900

    modify command harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -11,7 +11,7 @@ return {
     local keymap = vim.keymap.set
 
     -- Add file to Harpoon list (末尾に追加)
-    keymap('n', '<leader>a', function()
+    keymap('n', '<leader>ha', function()
       harpoon:list():add()
       vim.notify('Added to Harpoon: ' .. vim.fn.expand '%:t', vim.log.levels.INFO)
     end, { desc = 'Add file to Harpoon' })
@@ -29,23 +29,6 @@ return {
     end, { desc = 'Toggle Harpoon menu' })
 
     -- Quick jump to marked items (h1-4でジャンプ)
-    keymap('n', 'h1', function()
-      harpoon:list():select(1)
-    end, { desc = 'Jump to Harpoon mark 1' })
-
-    keymap('n', 'h2', function()
-      harpoon:list():select(2)
-    end, { desc = 'Jump to Harpoon mark 2' })
-
-    keymap('n', 'h3', function()
-      harpoon:list():select(3)
-    end, { desc = 'Jump to Harpoon mark 3' })
-
-    keymap('n', 'h4', function()
-      harpoon:list():select(4)
-    end, { desc = 'Jump to Harpoon mark 4' })
-
-    keymap('n', '<leader>hh', function() end, { desc = 'Jump to Harpoon mark' })
 
     keymap('n', '<leader>hh1', function()
       harpoon:list():select(1)
@@ -91,6 +74,20 @@ return {
       list:replace_at(4, list.config.create_list_item(list.config, current_file))
       vim.notify('Set Harpoon mark 4: ' .. vim.fn.expand '%:t', vim.log.levels.INFO)
     end, { desc = 'Set Harpoon mark 4' })
+
+    keymap('n', '<leader>h5', function()
+      local list = harpoon:list()
+      local current_file = vim.api.nvim_buf_get_name(0)
+      list:replace_at(5, list.config.create_list_item(list.config, current_file))
+      vim.notify('Set Harpoon mark 5: ' .. vim.fn.expand '%:t', vim.log.levels.INFO)
+    end, { desc = 'Set Harpoon mark 5' })
+
+    keymap('n', '<leader>h6', function()
+      local list = harpoon:list()
+      local current_file = vim.api.nvim_buf_get_name(0)
+      list:replace_at(6, list.config.create_list_item(list.config, current_file))
+      vim.notify('Set Harpoon mark 6: ' .. vim.fn.expand '%:t', vim.log.levels.INFO)
+    end, { desc = 'Set Harpoon mark 6' })
 
     -- Navigate between marked items
     keymap('n', '<A-h>', function()
