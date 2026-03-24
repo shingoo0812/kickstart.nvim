59f3c5bf68e882c6bd27354c3cbac90245d64e65
shingoo0812 <shingoo0812@gmail.com>
2026-03-25 08:43:40 +0900

    molti cursor

--- lua/plugins/Editing/visual-multi.lua
+++ lua/plugins/Editing/visual-multi.lua
@@ -1,9 +1,17 @@
 return {
   'mg979/vim-visual-multi',
   init = function()
+    vim.g.VM_default_mappings = 0
     vim.g.VM_maps = {
       ['Find Under'] = '<C-d>',
       ['Find Subword Under'] = '<C-S-d>',
+      -- ['Visual Cursors'] = '<C-d>',
+      -- ['Visual Add'] = '<C-S-d>',
+      ['Exit'] = '<Esc>',
     }
+  end,
+  config = function()
+    vim.keymap.set('x', '<leader>mc', '<Plug>(VM-Visual-Cursors)', { silent = true })
+    vim.keymap.set('x', '<leader>ma', '<Plug>(VM-Visual-Add)', { silent = true })
   end,
 }
